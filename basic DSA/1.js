fdf06bcc8e19cc4e24799856008e634c3d1dd239
Vanshika Kathpal <vanshikakathpal381@gmail.com>
2026-07-05 23:37:00 +0530

    solved question 1

--- basic DSA/1.js
+++ basic DSA/1.js
@@ -1,6 +1,6 @@
 
 //    1. Print 1 to N: Write a loop that prints numbers from 1 to a given number N.
-
-for(let i = 1; i <= navigation; i++){
-    console.log(i)
-}
+// const n = 10; 
+// for(let i = 1; i <= n; i++){
+//     console.log(i)
+// }
