2626ba734f022fa3d2cda5d284f197c6b612bb06
Vanshika Kathpal <vanshikakathpal381@gmail.com>
2026-07-06 00:03:54 +0530

    solved 2 question

--- basic DSA/1.js
+++ basic DSA/1.js
@@ -4,3 +4,4 @@
 // for(let i = 1; i <= n; i++){
 //     console.log(i)
 // }
+
---
+++ basic DSA/2.js
@@ -0,0 +1,11 @@
+//   2. Sum of Natural Numbers: Modify the previous loop to calculate and return the sum of all numbers from 1 to $N$.
+
+// let num = 10;
+// let sum = 0; 
+// for(let i = 1; i <=10; i++){
+//     sum+= i;  
+// }
+
+// console.log(sum)
+
+
