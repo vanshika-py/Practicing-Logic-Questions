6daf78fa376ce1e0836bc731f815363994af056f
Vanshika Kathpal <vanshikakathpal381@gmail.com>
2026-07-07 16:45:38 +0530

    solved 4 ques

--- basic DSA/3.js
+++ basic DSA/3.js
@@ -1,13 +1,13 @@
 // 3. Even or Odd: Write a conditional block that checks if a single integer input is even or odd without using loops.
 
 
-function test(Char){
-        if(Char%2 === 0){
-          console.log("even")
-        } else {
-           console.log("odd")
-        }
-    }
-test(205);
+// function test(Char){
+//         if(Char%2 === 0){
+//           console.log("even")
+//         } else {
+//            console.log("odd")
+//         }
+//     }
+// test(205);
 
 
--- basic DSA/4.js
+++ basic DSA/4.js
@@ -1,1 +1,10 @@
 //   4. Print Even Numbers up to N: Combine a loop and a conditional statement to print only the even numbers between 1 and $N$.
+
+// let n = 100; 
+
+// for(let i = 1; i <= n; i++){
+//     if(i%2 === 0){
+//         console.log(i)
+//     }
+// }
+
