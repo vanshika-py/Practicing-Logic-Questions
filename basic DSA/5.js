e4171935b6876c02951c66a28737d615f9a437a2
Vanshika Kathpal <vanshikakathpal381@gmail.com>
2026-07-07 17:09:18 +0530

    solved 5 ques

--- basic DSA/5.js
+++ basic DSA/5.js
@@ -1,1 +1,12 @@
 // 5. Count Multiples: Count how many numbers between 1 and $N$ are perfectly divisible by a user-specified divisor $K$.
+
+
+// let n = 10;
+// let k = 3
+
+
+// for(let i = 1; i <= n; i++){
+//     if( i %k === 0){
+//         console.log(i)
+//     }
+// }
