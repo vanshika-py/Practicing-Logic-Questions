be3e63d02c6b3a4013be31508a52f540eab28bba
Vanshika Kathpal <vanshikakathpal381@gmail.com>
2026-07-09 00:28:49 +0530

    feat: changes in variable name in code

--- basic DSA/7.js
+++ basic DSA/7.js
@@ -1,12 +1,12 @@
 // 7. Factorial of a Number: Write an iterative loop to calculate the product of all integers from 1 to $N$ (Factorial).
 
 let n = 6; 
-let product = 1;
+let product = 2;
 
 for(let i = 1; i <= n; i++){
     product*= i
 }
-console.log(m);
+console.log(product);
 
 
 
---
+++ basic DSA/8.js
@@ -0,0 +1,2 @@
+// 8. Power of a Number: Calculate $A$ raised to the power of $B$ ($A^B$) using a basic sequential multiplication loop
+
