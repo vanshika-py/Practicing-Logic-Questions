772e7cc977fd4472f1ef60d7d79c24a263cd5096
Vanshika Kathpal <vanshikakathpal381@gmail.com>
2026-07-09 00:05:55 +0530

    fixed issue

---
+++ basic DSA/6.js
@@ -0,0 +1,62 @@
+// 6. FizzBuzz Classic: Print numbers 1 to $N$. For multiples of 3 print "Fizz", for multiples of 5 print "Buzz", and for both print "FizzBuzz".
+
+// let n = 20;
+
+// for(let i = 1; i <=n; i++){
+//     if(i%3 === 0){
+//         console.log( i + "fizz")
+       
+//     } else if(i%5 === 0){
+//         console.log( i + "buzz")
+       
+//     } if (i%3 === 0 && i%5 === 0) {
+//         console.log("Fizzbuzz")
+//     } 
+//     }
+
+    
+
+
+
+
+let n = 20;
+
+for(let i = 1; i <= n; i++){
+    if (i%3 === 0 && i%5 === 0) {
+        console.log("Fizzbuzz")
+     } else if(i%3 === 0){
+        console.log("fizz")
+       
+    } else if(i%5 === 0){
+        console.log( "buzz")
+       
+    } else {
+        console.log(i)
+    }
+    } 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+   
+
+
+
+
+
+
