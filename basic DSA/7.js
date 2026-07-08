ba6cdccac429195a007155fd44a49fb897d7e1c0
Vanshika Kathpal <vanshikakathpal381@gmail.com>
2026-07-09 00:25:49 +0530

    solved 7th ques

---
+++ basic DSA/7.js
@@ -0,0 +1,47 @@
+// 7. Factorial of a Number: Write an iterative loop to calculate the product of all integers from 1 to $N$ (Factorial).
+
+let n = 6; 
+let product = 1;
+
+for(let i = 1; i <= n; i++){
+    product*= i
+}
+console.log(m);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
