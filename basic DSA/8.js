b1f1c594c9edd37bcec9952a471d97de847ec927
Vanshika Kathpal <vanshikakathpal381@gmail.com>
2026-07-14 12:02:22 +0530

    solved

--- basic DSA/8.js
+++ basic DSA/8.js
@@ -1,2 +1,3 @@
-// 8. Power of a Number: Calculate $A$ raised to the power of $B$ ($A^B$) using a basic sequential multiplication loop
+//  Power of a Number: Calculate $A$ raised to the power of $B$ ($A^B$) using a basic sequential multiplication loop
+
 
