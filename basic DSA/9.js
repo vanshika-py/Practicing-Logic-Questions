a9e870c26d30cb570a72c940b52f641c4e0b9e2c
Vanshika Kathpal <vanshikakathpal381@gmail.com>
2026-07-15 08:34:37 +0530

    changes in the code

--- basic DSA/9.js
+++ basic DSA/9.js
@@ -1,9 +1,21 @@
 // 9. Find All Factors: Find and print every integer that divides a given number $N$ perfectly without leaving a remainder.
 
-let n = 10;
+// let n = 10;
 
-for(let i = 1; i <= n; i++){
-   if(n%i === 0){
-console.log(i)
+// for(let i = 1; i <= n; i++){
+//    if(n%i === 0){
+// console.log(i)
+// }
+// }
+
+
+function num(n){
+
+    for(let i = 1; i <= n; i++){
+        if(n%i === 0){
+            console.log(i)
+        }
+    }
 }
-}
+
+num(25);
