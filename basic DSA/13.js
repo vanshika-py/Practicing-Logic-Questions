ca1aaaa736c62f014f9795d9e57ae647c4521a2d
Vanshika Kathpal <vanshikakathpal381@gmail.com>
2026-08-04 14:44:26 +0530

    solved ques character count frequency

--- basic DSA/13.js
+++ basic DSA/13.js
@@ -1,1 +1,59 @@
 //   13. Character Frequency (Targeted): Count how many times one specific character $C$ appears inside a given string.
+
+
+let str = "Ananya";
+let char = "a";
+let count = 0; 
+
+for(let i = 0;
+     i < str.length; i++){
+if(str[i].toLowerCase() === char.toLowerCase()){
+    count++
+}
+}
+console.log(count);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let count = 0
+
+// for(let i = 0; i < str.length; i++){
+
+//    console.log(str[i])
+
+
+// for(let j = 0; j < str.length; j++){
+//     if(str[j] === str[i]){
+//         count===  1
+//     }
+// }
+// console.log(count)
+
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
